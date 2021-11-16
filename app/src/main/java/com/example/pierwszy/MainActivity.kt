bcc7bba7134673082f3be5c0c6f407ac3104517e
coli98 <marcin.balchanowski@gmail.com>
2021-11-16 21:39:42 +0100

    na moim branchu v1

--- app/src/main/java/com/example/pierwszy/MainActivity.kt
+++ app/src/main/java/com/example/pierwszy/MainActivity.kt
@@ -14,7 +14,10 @@ class MainActivity : AppCompatActivity() {
         Log.d(this.javaClass.simpleName, "Wlasnie ruszyla")
 
         var number = 500
-        var bedziebrzydo = 2000
+
+ 
+        var bedziebrzydo = 3
+
         val button: Button = findViewById(R.id.button)
         val textField: TextView = findViewById(R.id.text)
 
